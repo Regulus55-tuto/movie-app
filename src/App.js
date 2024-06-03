6b35dbb053c51b2616d69311e378de34aaad61fb
regulus55 <hakjoon55@gmail.com>
2024-06-03 20:06:40 +0900

    map function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, {useState} from 'react';
 
 const App = () => {
-    const [name, setName] = useState("1234")
+    const [movies, setMovies] = useState([
+        {"title": "범죄도시1", "desc":"설명1","category":"카테고리1"},
+        {"title": "범죄도시2", "desc":"설명2","category":"카테고리2"},
+        {"title": "범죄도시3","desc":"설명3","category":"카테고리3"},
+        {"title": "범죄도시4","desc":"설명4","category":"카테고리4"},
+    ])
 
-    const changeName = () => {
-        setName("5678")
-    }
+
 
     return (
         <div>
-            {name}
-            <button onClick={() => changeName()}> 바꾸기 </button>
+            {movies && movies.map((movie)=> (
+                <div>
+                    <h1>{movie.title}</h1>
+                    <h2>{movie.desc}</h2>
+                    <h3>{movie.category}</h3>
+                </div>
+            ))}
         </div>
     );
 };
