ae6feff93f5a1ddb2dcfc255cbdebc5f9c1a9895
regulus55 <hakjoon55@gmail.com>
2024-06-03 19:46:03 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+    const [name, setName] = useState("1234")
+
+    const changeName = () => {
+        setName("5678")
+    }
+
     return (
         <div>
-            
+            {name}
+            <button onClick={() => changeName()}> 바꾸기 </button>
         </div>
     );
 };
